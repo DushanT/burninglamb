cb1f37e21090e224ffff1338b9656a8bd440dd1f
Dusan Tuzinsky <brooklynPD@gmail.com>
2024-04-03 12:16:14 +0200

    fix missing key

--- src/components/HorizontalScroll.js
+++ src/components/HorizontalScroll.js
@@ -13,17 +13,19 @@ export default function HorizontalScroll() {
   return (
     <div className="-mx-4 overflow-hidden border-y-0.5 border-text-900 pb-5 pt-7 md:mx-0 md:pb-8 md:pt-10">
       <div className="leading-1.3 tracking-2 flex w-[max-content] gap-4 whitespace-nowrap px-0 font-heading text-2xl lowercase motion-safe:animate-hscroll md:gap-8 md:text-6xl">
-        {[1, 2, 3, 4].map(() => (
-          <>
-            <span className="flex items-center gap-4 md:gap-8">
-              <span>Who are we</span>
-              <Arrow />
-              <span>and</span>
-              <Arrow />
-              <span>how can we help?</span>
-              <Arrow />
-            </span>
-          </>
+        {[1, 2, 3, 4].map((n) => (
+          <span
+            key={n}
+            aria-hidden={n > 1}
+            className="flex items-center gap-4 md:gap-8"
+          >
+            <span>Who are we</span>
+            <Arrow />
+            <span>and</span>
+            <Arrow />
+            <span>how can we help?</span>
+            <Arrow />
+          </span>
         ))}
       </div>
     </div>
