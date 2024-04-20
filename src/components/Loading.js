519371a85447bf7b42498de46f657376564329c0
Dusan Tuzinsky <brooklynPD@gmail.com>
2024-04-20 20:32:00 +0200

    Update spacing on loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,8 +2,10 @@ export default function Loading() {
   return (
     <div className="animate-slideout fixed left-0 top-0 z-10 grid h-full w-full place-items-center bg-[var(--bg-secondary)]">
       <div className="grid justify-items-center">
-        <div className="loading-logo mb-8" />
-        <p className="font-heading text-3xl lowercase">Burning lamb</p>
+        <div className="loading-logo mb-5" />
+        <p className="font-heading text-3xl lowercase tracking-2">
+          Burning lamb
+        </p>
       </div>
     </div>
   );
